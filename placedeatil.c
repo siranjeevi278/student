36cde8afd689a1b703a7117286563581ca53901f
siranjeevi278 <siranjeevis.20cse@kongu.edu>
2021-10-23 11:16:29 +0530

    placement commit

---
+++ placedeatil.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+char name[23],rollno[56],personalmail[34],edumailid[78];
+int mobileno,int days_or_hostel,no_of_arrears,gpa1,gpa2,gpa3,gpa4;
+printf("enter name");
+scanf("%s",name);
+printf("enter rollno");
+printf("enter personalmail");
+scanf("%s",personalmail);
+printf("enter edumail");
+scanf("%s",edumail);
+printf("enter mobileno");
+scanf("%d",&mobileno);
+printf("enter dausscholar or hosteller");
+scanf("%d",&days_or_hostel);
+printf("enter no of arrears");
+scanf("%d",&no_of_arrears);
+printf("enter gpa1");
+scanf("%d",&gpa1);
+printf("enter gpa2");
+scanf("%d",&gpa2);
+printf("enter gpa3");
+scanf("%d",&gpa3);
+
+
