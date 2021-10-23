5fc29e143d8da4f1a93411d79388ef283aa3aa93
siranjeevi278 <siranjeevis.20cse@kongu.edu>
2021-10-23 11:03:36 +0530

    insurance commit

---
+++ insdetails.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+char studentname[34],nomineename[45],reationship[12],nomineeoccupation[23],nomineeemail[45];
+int age,nomineemobileno,naadhear;
+printf("enter student name");
+scanf("%s",studentname);
+printf("enter nomineename");
+scanf("%s",nomineename);
+printf("enter relationship");
+scanf("%s",relationship);
+printf("enter b=nomineeoccupation");
+scanf("%s",nomineeoccupation);
+printf("enter nomineeemail");
+scanf("%s",nomineeemail);
+printf("enter age");
+scanf("%d",&age);
+printf("enter nomineemobile no");
+scanf("%d",&nomineemobileno);
+printf("enter nominee aadher no:");
+scanf("%d",&naadharno);
+
