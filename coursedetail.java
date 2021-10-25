0675b4101ba5ac04845a624dc223333b1377deb3
siranjeevi278 <siranjeevis.20cse@kongu.edu>
2021-10-25 19:13:13 +0530

    coursecomplete

---
+++ coursedetail.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class course
+{
+	public static void main(String args[])
+	{
+		Scannner s=new Scanner(System.in);
+		String rollno=s.next();
+		int number =s.nextInt();
+		String no_of_creditcourses=s.nextInt();
+                int no_of_value_courses=s.nextInt();
+		name_valueadded_course=s.nextInt();
+
+
