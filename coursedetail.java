466b3e23b7ad30a921ed440483f28bae8dddab0d
siranjeevi278 <91017254+siranjeevi278@users.noreply.github.com>
2021-10-26 18:48:01 +0530

    last commit

--- coursedetail.java
+++ coursedetail.java
@@ -4,10 +4,15 @@ class course
 	public static void main(String args[])
 	{
 		Scannner s=new Scanner(System.in);
+		System.out.println("enter rollno:");
 		String rollno=s.next();
+		System.out.println("enter number:");
 		int number =s.nextInt();
+		System.out.println("enter no of credit courses":");
 		String no_of_creditcourses=s.nextInt();
+				   System.out.println("enter no of value added courses:");
                 int no_of_value_courses=s.nextInt();
+				   System.out.println("enter name of value added courses:");
 		name_valueadded_course=s.nextInt();
 
 
